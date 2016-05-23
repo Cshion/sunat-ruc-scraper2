b09e8e2ad83d7bb657a78e749f142f349892dcd4
Cshion <aaron.stbn@gmail.com>
2016-05-23 17:51:32 -0500

    fist steps to multiple ruc

--- test/scraper.js
+++ test/scraper.js
@@ -2,50 +2,61 @@ var expect  = require("chai").expect;
 var scraper = require("../lib");
 
 describe("Sunat Ruc Scraper" , function () {
-	it("Get personal information from valid ruc" , function (cb) {
-		scraper.getInformation("20131312955" , function (error , data) {
-			if(error){
+	/*
+	 it("Get personal information from valid ruc" , function ( cb ) {
+	 scraper.getInformation("20131312955" , function ( error , data ) {
+	 if ( error ) {
+	 throw error;
+	 }
+	 expect(data).to.be.not.empty;
+	 expect(data).to.be.object;
+	 expect(data.ruc).to.be.not.empty;
+	 expect(data.razon_social).to.be.not.empty;
+	 return cb();
+	 });
+	 });
+
+	 it("Get empty information from not valid ruc" , function ( cb ) {
+	 scraper.getInformation("201313129551" , function ( error , data ) {
+	 if ( error ) {
+	 throw error;
+	 }
+	 expect(data).to.be.empty;
+	 return cb();
+	 });
+	 });
+
+	 it("Get extended information from valid ruc" , function ( cb ) {
+	 scraper.getAllInformation("20131312955" , function ( error , data , body ) {
+	 if ( error ) {
+	 throw error;
+	 }
+	 expect(data).to.be.not.empty;
+	 expect(data).to.be.object;
+	 expect(data.sistema_emision).to.be.not.empty;
+	 expect(data.comercio_exterior).to.be.not.empty;
+	 return cb();
+	 })
+	 });
+
+	 it("Get empty extended information from not valid ruc" , function ( cb ) {
+	 scraper.getAllInformation("201313129551" , function ( error , data , body ) {
+	 if ( error ) {
+	 throw error;
+	 }
+	 expect(data).to.be.empty;
+	 return cb();
+	 })
+	 });
+	 */
+	it("Get personal information from valid set of ruc's" , function ( cb ) {
+		scraper.getInformation([ "20131312955" , "20601156530" ] , function ( error , data ) {
+			if ( error ) {
 				throw error;
 			}
-			expect(data).to.be.not.empty;
-			expect(data).to.be.object;
-			expect(data.ruc).to.be.not.empty;
-			expect(data.razon_social).to.be.not.empty;
+			
 			return cb();
 		});
-	});
-
-	it("Get empty information from not valid ruc" , function (cb) {
-		scraper.getInformation("201313129551" , function (error , data) {
-			if(error){
-				throw error;
-			}
-			expect(data).to.be.empty;
-			return cb();
-		});
-	});
-
-	it("Get extended information from valid ruc" , function (cb) {
-		scraper.getAllInformation("20131312955" , function (error , data , body) {
-			if(error){
-				throw error;
-			}
-			expect(data).to.be.not.empty;
-			expect(data).to.be.object;
-			expect(data.sistema_emision).to.be.not.empty;
-			expect(data.comercio_exterior).to.be.not.empty;
-			return cb();
-		})
-	});
-
-	it("Get empty extended information from not valid ruc" , function (cb) {
-		scraper.getAllInformation("201313129551" , function (error , data , body) {
-			if(error){
-				throw error;
-			}
-			expect(data).to.be.empty;
-			return cb();
-		})
 	});
 
 });
